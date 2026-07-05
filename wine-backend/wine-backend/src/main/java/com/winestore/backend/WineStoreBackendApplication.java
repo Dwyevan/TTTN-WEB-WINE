5dec6c3900fc2ef04161c57860fd4fdefd95a2c1
Dwyevan <hanhatduy28805@gmail.com>
2026-07-05 14:23:36 +0700

    Set default JVM timezone to Asia/Ho_Chi_Minh

--- wine-backend/wine-backend/src/main/java/com/winestore/backend/WineStoreBackendApplication.java
+++ wine-backend/wine-backend/src/main/java/com/winestore/backend/WineStoreBackendApplication.java
@@ -2,12 +2,19 @@ package com.winestore.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class WineStoreBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WineStoreBackendApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 	}
 
 }
